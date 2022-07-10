d15655e5870abb134879d495ad468c2d593a0454
Vu Anh <anhv.ict91@gmail.com>
2022-07-10 19:09:20 +0700

    update

--- ci.sh
+++ ci.sh
@@ -1,9 +1,9 @@
 rm -rf Build
-rm -rf html
 rm -rf StreamingAssets
 rm -rf TemplateData
 cp "/Users/anhv/My project/html/" html 
 mv html/* .
+rm -rf html
 git status
 git add -A
 git commit -m 'update'
